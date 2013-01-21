214f7c42041e2dd51c471e4a8b1fa3b3841985b4
Ikey Doherty <contactjfreak@gmail.com>
2013-01-21 20:27:47 +0000

    Update to consortium

--- usr/lib/solusos/solusDesktop/metacity_preview.py
+++ usr/lib/solusos/solusDesktop/metacity_preview.py
@@ -3,14 +3,14 @@ import pygtk
 import gtk
 import dbus
 import dbus.service
-import metacity
+import consortium
 
 from dbus.mainloop.glib import DBusGMainLoop
 
-class MetacityThemePreview(dbus.service.Object):
+class ConsortiumThemePreview(dbus.service.Object):
 
    def __init__(self):
-	bus_name = dbus.service.BusName('com.solusos.metacitythemepreview', bus=dbus.SessionBus())
+	bus_name = dbus.service.BusName('com.solusos.consortiumthemepreview', bus=dbus.SessionBus())
 
 	# make plug, reparent
 	self.plug = gtk.Plug(0L)
@@ -22,23 +22,23 @@ class MetacityThemePreview(dbus.service.Object):
 
 	self.theme = None
 
-	dbus.service.Object.__init__(self, bus_name, '/com/solusos/metacitythemepreview')
+	dbus.service.Object.__init__(self, bus_name, '/com/solusos/consortiumthemepreview')
 
    def create_ui(self, some_event):
-	self.preview = metacity.Preview()
+	self.preview = consortium.Preview()
 	self.plug.add(self.preview)
 	self.plug.show_all()	
 	return
 
-   @dbus.service.method(dbus_interface = "com.solusos.metacitythemepreview", in_signature = None, out_signature = 'i')
+   @dbus.service.method(dbus_interface = "com.solusos.consortiumthemepreview", in_signature = None, out_signature = 'i')
    def get_plug_id(self):
 	return self.plug_id
 
-   @dbus.service.method(dbus_interface = "com.solusos.metacitythemepreview", in_signature = 's', out_signature = None)
+   @dbus.service.method(dbus_interface = "com.solusos.consortiumthemepreview", in_signature = 's', out_signature = None)
    def set_theme_name(self, theme_name):
 	if self.theme is not None:
 		self.theme.free()
-	theme = metacity.theme_load(theme_name)
+	theme = consortium.theme_load(theme_name)
 	self.preview.set_title("%s theme" % theme_name)
 	self.preview.set_theme(theme)
 
@@ -47,6 +47,6 @@ class MetacityThemePreview(dbus.service.Object):
 
 if __name__ == "__main__":
 	DBusGMainLoop(set_as_default=True)
-	myservice = MetacityThemePreview()
+	myservice = ConsortiumThemePreview()
 	gtk.main()
 
